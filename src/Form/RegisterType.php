1f91ac1e581a5773a28f7916968386781a0fd36a
Wingsdestiny77 <lys.phorimavong.alex@gmail.com>
2021-05-09 23:09:02 +0200

    Add : RegisterType

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -7,26 +7,27 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class WritingType extends AbstractType
+class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('id', HiddenType::class)
-            ->add('title', TextType::class, [
+            ->add('username', TextType::class, [
                 'label' => false,
             ])
-            ->add('content', TextareaType::class, [
+            ->add('password', PasswordType::class, [
                 'label' => false,
-                'attr' => [
-                    'class' => 'tinymce']
-            ]);
+            ])
+            ->add('email', EmailType::class, [
+                'label' => false]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
