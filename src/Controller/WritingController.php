c986223a35bbcf6bb1e88cb06d53b820d1c9628c
Wingsdestiny77 <lys.phorimavong.alex@gmail.com>
2021-05-11 20:43:38 +0200

    Update : documentation du controller

--- src/Controller/WritingController.php
+++ src/Controller/WritingController.php
@@ -19,10 +19,14 @@ class WritingController extends AbstractController
     public function index(EntityManagerInterface $em, Request $request): Response
     {
 
+        // Initialisation du formulaire
         $form =$this->createForm(WritingType::class);
         $form->handleRequest($request);
 
+        // Soumission et validation du formulaire
         if ($form->isSubmitted() && $form->isValid()){
+
+            // Création d'une histoire
             $writing = new Writing();
             $writing->setTitle($form->get('title')->getData());
             $writing->setContent($form->get('content')->getData());
@@ -30,6 +34,8 @@ class WritingController extends AbstractController
             $writing->setUser($this->getUser()->getId());
             $em->persist($writing);
             $em->flush();
+
+            // Message de validation + redirection
             $this->addFlash('success','Enregistrement réussi.');
             return $this->redirectToRoute('writing');
         }
@@ -43,13 +49,18 @@ class WritingController extends AbstractController
      * @Route("/writing/list", name="writing.list")
      * @Route("/writing/list/{userId}", name="writing.list_user", requirements={"userId"="[0-9+]"})
      * @Route("/writing/list/{userId}/{storyId}", name="writing.delete", requirements={"userId"="[0-9+]","storyId"="[0-9+]"})
-     *
      */
     public function list($userId = null, $storyId = null, EntityManagerInterface $em, Request $request): Response
     {
+
+        // Récupération du repo Writing
         $this->writingRepo = $this->getDoctrine()->getRepository(Writing::class);
+
+        // Condition qui permet de récupérer toutes les histoires ou seulement les histoires de l'utilisateur
         if ($userId == $this->getUser()->getId()){
             $writing = $this->writingRepo->findBy(['user' => $this->getUser()->getId()]);
+
+            // Suppression d'une histoire
             if ($storyId){
                 $writing = $this->writingRepo->find($storyId);
                 $em->remove($writing);
@@ -62,16 +73,23 @@ class WritingController extends AbstractController
             $writing = $this->writingRepo->findAll();
         }
 
+        // Initialisation du formulaire
         $form = $this->createForm(WritingType::class);
         $form->handleRequest($request);
 
+
+        // Soumission et validation du formulaire
         if ($form->isSubmitted() && $form->isValid()){
+
+            // Modification de l'histoire
             $story = $this->writingRepo->find($form->get('id')->getData());
             $story->setTitle($form->get('title')->getData());
             $story->setContent($form->get('content')->getData());
             $story->setUpdateDate(new \DateTime());
             $em->persist($story);
             $em->flush($story);
+
+            // Message de validation + redirection
             $this->addFlash('success', 'Mise à jour réussie');
             return $this->redirectToRoute('writing.list');
         }
