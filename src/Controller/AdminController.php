8f582b7f02d3700e802a2fe691014eeef5d526e4
Wingsdestiny77 <lys.phorimavong.alex@gmail.com>
2021-05-23 19:31:23 +0200

    Add : AdminController

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\Writing;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        // Recupération des repository
+        $this->writingRepo = $this->getDoctrine()->getRepository(Writing::class);
+        $this->userRepo = $this->getDoctrine()->getRepository(User::class);
+
+
+
+        $writing = $this->writingRepo->findAll();
+        $user = $this->userRepo->findAll();
+        foreach ($writing as $keyWriting => $valueWriting){
+            foreach ($user as $keyUser => $valueUser){
+                if ($valueWriting->getUser() == $valueUser->getId()){
+                    $writing[$keyWriting]->username = $valueUser->getUsername();
+                }
+            }
+        }
+
+        return $this->render('admin/index.html.twig', [
+            'writings' => $writing??null,
+            'users' => $user??null,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/{userId}", name="admin.delete_user", requirements={"userId"="[0-9]+"})
+     * @Route("/admin/writing/{storyId}", name="admin.delete_writing", requirements={"storyId"="[0-9]+"})
+     */
+    public function delete(Request $request, EntityManagerInterface $em, $userId = null, $storyId = null): Response
+    {
+        // Recupération des repository
+        $this->userRepo = $this->getDoctrine()->getRepository(User::class);
+        $this->writingRepo = $this->getDoctrine()->getRepository(Writing::class);
+
+        // Récupérer la route actuelle
+        $route = $request->attributes->get('_route');
+
+        if ($route == 'admin.delete_user'){
+            $user = $this->userRepo->find($userId);
+            $em->remove($user);
+            $em->flush();
+            $this->addFlash('success', 'L\'utilisateur a été supprimé.');
+        }
+
+        if ($route == 'admin.delete_writing'){
+            $writing = $this->writingRepo->find($storyId);
+            $em->remove($writing);
+            $em->flush();
+            $this->addFlash('success', 'L\'histoire a été supprimée.');
+        }
+
+        return $this->redirectToRoute('admin');
+    }
+}
