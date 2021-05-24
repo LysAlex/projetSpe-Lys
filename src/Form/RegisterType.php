7fc870fcb3046e37a0a184704ea43e9c02f2f76e
Wingsdestiny77 <lys.phorimavong.alex@gmail.com>
2021-05-24 19:36:23 +0200

    Update : image as FileType

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -2,18 +2,21 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use App\Entity\Writing;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class RegisterType extends AbstractType
 {
@@ -27,12 +30,28 @@ class RegisterType extends AbstractType
                 'label' => false,
             ])
             ->add('email', EmailType::class, [
-                'label' => false]);
+                'label' => false])
+            ->add('image', FileType::class, [
+                'label' => false,
+                'data_class' => null,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PNG / JPEG image',
+                    ])
+                ],
+            ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
+            'empty_data' => null,
         ]);
     }
 }
